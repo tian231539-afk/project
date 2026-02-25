854e4e0f6054247e136c78b82cea1464177d4837
tian231539-afk <tian231539@gmail.com>
2026-02-25 16:55:03 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function DatingChatAssistant() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (chatHistory || myProfile || theirProfile) {
+      if (chatHistory || myProfile || otherProfile) {
         localStorage.setItem(STORAGE_KEY, JSON.stringify({
           chatHistory, myProfile, theirProfile, selectedStyle,
         }));
